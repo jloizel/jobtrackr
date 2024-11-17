6528faf05fb85a434e8fde1886af4b63e799b93e
Jack <102869405+jloizel@users.noreply.github.com>
2024-11-17 23:32:58 +0000

    working on navbar styling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,22 @@
 import Authentication from "../authentication/authentication";
+import ThemeToggle from "../themeToggle/themeToggle";
 import styles from "./navbar.module.css"
+import { AiOutlineSchedule } from "react-icons/ai";
+import { GiFishingLure } from "react-icons/gi";
+import { GiFishing } from "react-icons/gi";
 
 const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
-      <div className={styles.title}>
-        <span>Job</span>
-        <span>Trackr</span>
-      </div>
+      <ThemeToggle/>
+      <a className={styles.title} href={"/"}>
+        <GiFishing className={styles.icon}/>
+        <div className={styles.text}>
+          <span>Job</span>
+          <span>Trackr</span> 
+        </div>
+      </a>
       <Authentication/>
     </div>
  );
