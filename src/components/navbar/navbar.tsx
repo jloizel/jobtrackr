7f015ad354ef683c665d39f6a1cfe08fb0a44e85
Jack <102869405+jloizel@users.noreply.github.com>
2024-11-19 23:57:30 +0000

    mobile design on navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,15 +1,32 @@
+import { GiFishing } from "react-icons/gi";
 import Authentication from "../authentication/authentication";
 import ThemeToggle from "../themeToggle/themeToggle";
 import styles from "./navbar.module.css"
-import { AiOutlineSchedule } from "react-icons/ai";
-import { GiFishingLure } from "react-icons/gi";
-import { GiFishing } from "react-icons/gi";
+import { createTheme, useMediaQuery } from '@mui/material';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 1024,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+const isTabletOrBelow = useMediaQuery(theme.breakpoints.down('md'));
+const isComputer = useMediaQuery(theme.breakpoints.up('md'));
 
 const Navbar = () => {
+  
 
   return (
     <div className={styles.navbar}>
-      
+
       <ThemeToggle/>
       <a className={styles.title} href={"/"}>
         <GiFishing className={styles.icon}/>
