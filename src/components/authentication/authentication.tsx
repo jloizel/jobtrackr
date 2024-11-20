d4ebf776a79cce870b748de5c926892b259a979e
Jack <102869405+jloizel@users.noreply.github.com>
2024-11-20 00:17:34 +0000

    started burger menu for authentication

--- src/components/authentication/authentication.tsx
+++ src/components/authentication/authentication.tsx
@@ -1,17 +1,44 @@
 import styles from "./authentication.module.css"
+import { IoIosMenu } from "react-icons/io";
+import { FiMenu } from "react-icons/fi";
+import { SlMenu } from "react-icons/sl";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { createTheme, useMediaQuery } from '@mui/material';
+import { useState } from "react";
 
 const Authentication = () => {
+  const [openModal, setOpenModal] = useState(false)
+
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 768,
+        md: 1024,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
+  });
+  
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={styles.authentication}>
-      <div className={styles.login}>
-        Log in
+      {isMobile ? (
+        <RxHamburgerMenu className={styles.menuIcon}/>
+      ):(
+        <div className={styles.buttonContainer}>
+          <div className={styles.login}>
+            Log in
+          </div>
+          <div className={styles.signup}>
+            Sign up
+          </div>
       </div>
-      <div className={styles.signup}>
-        Sign up
-      </div>
+      )}
     </div>
- );
+  );
 }
 
 export default Authentication
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,28 +2,9 @@ import { GiFishing } from "react-icons/gi";
 import Authentication from "../authentication/authentication";
 import ThemeToggle from "../themeToggle/themeToggle";
 import styles from "./navbar.module.css"
-import { createTheme, useMediaQuery } from '@mui/material';
-
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 768,
-      md: 1024,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-});
-
-const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-const isTabletOrBelow = useMediaQuery(theme.breakpoints.down('md'));
-const isComputer = useMediaQuery(theme.breakpoints.up('md'));
 
 const Navbar = () => {
   
-
   return (
     <div className={styles.navbar}>
 
