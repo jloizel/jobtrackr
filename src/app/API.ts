1953bf25ef52675fb8d8d2daa71795a06dc95d71
Jack <jmloizel@gmail.com>
2024-11-24 16:51:05 +0000

    Update API.ts

--- src/app/API.ts
+++ src/app/API.ts
@@ -1,7 +1,9 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const BASE_URL = 'https://natalies-next-stop-server.vercel.app/'; 
+const BASE_URL = 'https://jobtrackr-server.vercel.app/'; 
 
+
+// Create an Axios instance with custom configurations
 const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
@@ -9,140 +11,71 @@ const api: AxiosInstance = axios.create({
   },
 });
 
-export interface IContentBlock {
-  type: 'text' | 'image' | 'subheader' | 'list';
-  content: string;
-  subContent?: string[];
-  images?: string[];
-  imageCaption: string;
-  nestedBlocks?: IContentBlock[]; 
-  [key: string]: any;
-}
-
-export interface ISubsection {
-  header: string; 
-  text: string; 
-  images: string[]; 
-  imageCaption: string;
-  contentBlocks: IContentBlock[];
-  [key: string]: any;
-}
-
-export interface Post {
+// Define types for request data and response data
+export interface Job {
   _id: string;
-  countryImage: string;
+  userEmail: string;
+  satus: string;
   title: string;
-  desc: string;
-  introText: string; 
-  introImage: string; 
-  introImageLink: string;
-  introImageCaption: string;
-  previewImage: string;
-  subsections: ISubsection[]; 
-  continent: string;
-  country: string;
-  username: string;
-  createdAt: string;
-  updatedAt: string;
+  company: string;
+  salary: string;
+  location: string;
 }
 
-export type PostInput = Omit<Post, '_id' | 'username' | 'createdAt' | 'updatedAt'>;
-
-export interface Comment {
-  _id: string;
-  postId: string;
-  name: string;       
-  content: string;    
-  createdAt: string;  
-}
-
-// Create a new post
-export const createPost = async (postData: PostInput): Promise<Post> => {
+// API functions
+export const createJob = async (jobData: { position: string, contractType: string, location: string, specialisation: string, salary: string, jobDescription: string, duration: string, responsibilities: string[], skillsExperience: string[] }): Promise<Job> => {
   try {
-    const response: AxiosResponse<Post> = await api.post('/post/create', postData);
+    const response: AxiosResponse<Job> = await api.post('/jobs/create', jobData);
     return response.data;
   } catch (error) {
     throw error; 
   }
 };
 
-// Get a single post by ID
-export const getPostById = async (postId: string): Promise<Post | null> => {
+export const getJobById = async (jobId: string): Promise<Job | null> => {
   try {
-    // console.log(`Fetching post with ID: ${postId}`);
-    const response: AxiosResponse<Post> = await api.get(`/post/get/${postId}`);
-    return response.data; 
+    const response: AxiosResponse<{ job: Job }> = await api.get(`/jobs/get/${jobId}`);
+    return response.data.job;
   } catch (error) {
-    const axiosError = error as AxiosError;
-    console.error('Error fetching post:', axiosError);
-
-    // Check for 404 error
-    if (axiosError.response && axiosError.response.status === 404) {
+    if (error === 404) {
       return null; 
     }
-    throw error; 
+    throw error; // Throw other errors
   }
 };
 
-
-// Get all posts
-export const getAllPosts = async (): Promise<Post[]> => {
+export const getAllJobs = async (): Promise<Job[]> => {
   try {
-    const response: AxiosResponse<{ post: Post[] }> = await api.get('/post/get');
-    return response.data.post || [];
+    const response: AxiosResponse<{ jobs: Job[] }> = await api.get('/jobs/get');
+    return response.data.jobs || [];
   } catch (error) {
-    console.error('Error fetching posts:', error);
+    console.error('Error fetching jobs:', error);
     return [];
   }
 };
 
-// Get posts by continent
-export const getPostsByContinent = async (continent: string): Promise<Post[]> => {
-  // console.log(`Fetching posts for continent: ${continent}`);
-  try {
-    const response: AxiosResponse<Post[]> = await api.get(`/post/continent/${continent}`);
-    // console.log('Response from API:', response.data); 
-    return response.data; 
-  } catch (error) {
-    console.error('Error fetching posts by continent:', error);
-    return [];
-  }
-};
 
-// Get posts by continent and country
-export const getPostsByContinentAndCountry = async (continent: string, country: string): Promise<Post[]> => {
+export const updateJob = async (jobId: string, jobData: { position: string, contractType: string, location: string, specialisation: string, salary: string, jobDescription: string, duration: string, responsibilities: string[], skillsExperience: string[] }): Promise<Job> => {
   try {
-    const response: AxiosResponse<Post[]> = await api.get(`/post/continent/${continent}/country/${country}`);
-    return response.data || [];
-  } catch (error) {
-    console.error('Error fetching posts by continent and country:', error);
-    return [];
-  }
-};
-
-// Update a post by ID
-export const updatePost = async (postId: string, postData: Partial<PostInput>): Promise<Post> => {
-  try {
-    const response: AxiosResponse<Post> = await api.patch(`/post/update/${postId}`, postData);
+    const response: AxiosResponse<Job> = await api.patch(`/jobs/update/${jobId}`, jobData);
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-// Delete a post by ID
-export const deletePost = async (postId: string): Promise<{ message: string }> => {
+export const deleteJob = async (jobId: string): Promise<{ message: string }> => {
   try {
-    const response: AxiosResponse<{ post: Post; message: string }> = await api.delete(`/post/delete/${postId}`);
+    const response: AxiosResponse<{ job: Job; message: string }> = await api.delete(`/jobs/delete/${jobId}`);
     return { message: response.data.message };
   } catch (error) {
     throw error;
   }
 };
 
-export const sendEmail = async (formData: FormData): Promise<{ message: string }> => {
+export const sendCV = async (formData: FormData): Promise<{ message: string }> => {
   try {
-    const response: AxiosResponse<{ message: string }> = await api.post('/api/sendEmail', formData, {
+    const response: AxiosResponse<{ message: string }> = await api.post('/api/sendCV', formData, {
       headers: {
         'Content-Type': 'multipart/form-data', 
       },
@@ -153,57 +86,13 @@ export const sendEmail = async (formData: FormData): Promise<{ message: string }
   }
 };
 
-export const getInstagramPosts = async (): Promise<any[]> => {
+export const submitVacancy = async (formData: FormData): Promise<{ message: string }> => {
   try {
-    const response: AxiosResponse<{ posts: any[] }> = await api.get('/instagram/posts');
-    return response.data.posts;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    console.error('Error fetching Instagram posts:', axiosError);
-
-    // Check if the error is due to an expired token (401 Unauthorized)
-    if (axiosError.response && axiosError.response.status === 401) {
-      console.warn('Token expired, attempting to refresh...');
-      await refreshInstagramToken(); 
-      return await getInstagramPosts(); 
-    }
-    throw error; 
-  }
-};
-
-export const refreshInstagramToken = async (): Promise<void> => {
-  try {
-    await api.post('/instagram/refresh-token');
-    // console.log('Instagram token refreshed successfully');
-  } catch (error) {
-    console.error('Error refreshing Instagram token:', error);
-    throw error; 
-  }
-};
-
-export const addComment = async (commentId: string, name: string, content: string): Promise<Comment> => {
-  try {
-    const response: AxiosResponse<Comment> = await api.post('/comments', { commentId, name, content });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Get all comments for a specific post
-export const getCommentsByPostId = async (postId: string): Promise<Comment[]> => {
-  try {
-    const response: AxiosResponse<Comment[]> = await api.get(`/comments/${postId}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching comments:', error);
-    return [];
-  }
-};
-
-export const deleteComment = async (commentId: string): Promise<{ message: string }> => {
-  try {
-    const response: AxiosResponse<{ message: string }> = await api.delete(`/comments/delete/${commentId}`);
+    const response: AxiosResponse<{ message: string }> = await api.post('/api/submitVacancy', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data', 
+      },
+    });
     return response.data;
   } catch (error) {
     throw error; 
