54e929bb831a1479b347c8f6b210f8fd50337b7e
Jack <jmloizel@gmail.com>
2024-11-25 22:54:42 +0000

    Update navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       <div className={styles.rightContainer}>
       {status === "authenticated" && (
         <Link href="/trackr">
-          Trackr
+          Trackr 
         </Link>
       )}
         <Authentication/>
