f59326c759bbe1d47cd274fcaf889d860d398c3b
Jack <102869405+jloizel@users.noreply.github.com>
2025-02-02 23:43:00 +0000

    updating spline scene

--- src/components/splineScene/splineScene.tsx
+++ src/components/splineScene/splineScene.tsx
@@ -1,36 +1,52 @@
 import React, { useRef, useEffect, useState } from "react";
 import Spline from "@splinetool/react-spline";
-import styles from "./splineScene.module.css"
+import styles from "./splineScene.module.css";
+import { createTheme, useMediaQuery } from "@mui/material";
 
 const SplineScene = () => {
-  const [sceneKey, setSceneKey] = useState(0); 
-  const splineRef = useRef(null);
+  const splineRef = useRef<any>(null);
+  const [zoom, setZoom] = useState(1); // Default zoom level
 
-  function onLoad(splineApp:any) {
+  function onLoad(splineApp: any) {
     splineRef.current = splineApp;
+    updateZoom(); // Set initial zoom based on screen size
+  }
 
-    const camera = splineApp.currentCamera;
-    if (camera) {
-      camera.position.set(0, 0, 800); 
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 768,
+        md: 1024,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
+  });
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"))
+
+  function updateZoom() {
+    if (splineRef.current) {
+      const screenWidth = window.innerWidth;
+
+      let newZoom = 0.7;
+      if (isMobile) {
+        newZoom = 0.8;
+      } else if (isTablet) {
+        newZoom = 0.9;
+      } else {
+        newZoom = 2; 
+      }
+
+      setZoom(newZoom);
+      splineRef.current.setZoom(newZoom);
     }
   }
 
-  function resetScene() {
-    setSceneKey((prevKey) => prevKey + 1); // changing the key forces a re-render
-  }
-
-  // restart scene after a delay 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      resetScene(); 
-    }, 12000); // match full animation cycle
-
-    return () => clearInterval(interval); 
-  }, []);
-
   return (
     <Spline
-      key={sceneKey} 
       scene="https://prod.spline.design/ZDnufltbCL2CG535/scene.splinecode"
       onLoad={onLoad}
       className={styles.container}
